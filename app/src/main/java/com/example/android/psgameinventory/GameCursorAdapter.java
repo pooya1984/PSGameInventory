2889ed54cfad1d2fb4d094773a4bc7727e1ed79e
pooya1984 <pooya.nodehi@gmail.com>
2017-07-20 19:52:12 +0200

    initial commit

--- app/src/main/java/com/example/android/psgameinventory/GameCursorAdapter.java
+++ app/src/main/java/com/example/android/psgameinventory/GameCursorAdapter.java
@@ -11,11 +11,15 @@ import android.widget.TextView;
 
 import com.example.android.psgameinventory.data.GameContract.GameEntry;
 
+import static com.example.android.psgameinventory.R.id.console;
+
 public class GameCursorAdapter extends CursorAdapter {
 
     public GameCursorAdapter(Context context, Cursor c) {
         super(context, c, 0 /* flags */);
     }
+
+    private String GenreString;
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
@@ -27,20 +31,46 @@ public class GameCursorAdapter extends CursorAdapter {
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+        TextView genreTextView = (TextView) view.findViewById(R.id.genre);
+        TextView consoleTextView= (TextView)view.findViewById(console);
 
 
         // Find the columns of pet attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(GameEntry.COLUMN_GAME_NAME);
         int gameColumnIndex = cursor.getColumnIndex(GameEntry.COLUMN_GAME_STOCK);
+        int genreColumnIndex=cursor.getColumnIndex(GameEntry.COLUMN_GAME_GENRE);
+        int consoleColumnIndex=cursor.getColumnIndex(GameEntry.COLUMN_GAME_CONSOLE);
 
         // Read the pet attributes from the Cursor for the current pet
         String gameName = cursor.getString(nameColumnIndex);
         String gameStock = cursor.getString(gameColumnIndex);
+        String genre = cursor.getString(genreColumnIndex);
+        String console = cursor.getString(consoleColumnIndex);
+
+        String GenreString="";
+        String ConsoleString="";
+
+        switch (Integer.parseInt(genre)) {
+            case 1: GenreString = context.getString(R.string.SCI_FI);break;
+            case 2: GenreString=context.getString(R.string.ACTION);break;
+            case 3: GenreString=context.getString(R.string.SPORT);break;
+            case 4: GenreString=context.getString(R.string.ADVENTURE);break;
+            case 5: GenreString=context.getString(R.string.SHOOTING);break;
+        }
+
+        switch (Integer.parseInt(console)) {
+            case 1: GenreString = context.getString(R.string.PS);break;
+            case 2: GenreString=context.getString(R.string.PS1);break;
+            case 3: GenreString=context.getString(R.string.PS2);break;
+            case 4: GenreString=context.getString(R.string.PS3);break;
+            case 5: GenreString=context.getString(R.string.PS4);break;
+        }
 
         if (TextUtils.isEmpty(gameStock)) {
-            gameStock = context.getString(R.string.unknown_Price);
-        }
+            gameStock = context.getString(R.string.unknown_Price);}
         // Update the TextViews with the attributes for the current pet
         nameTextView.setText(gameName);
         summaryTextView.setText(gameStock);
+        genreTextView.setText(GenreString);
+        consoleTextView.setText(ConsoleString);
     }}
