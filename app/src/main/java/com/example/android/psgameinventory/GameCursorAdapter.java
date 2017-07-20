ac8b42621972b09814d23bf704e5f5e06eb296ba
pooya1984 <pooya.nodehi@gmail.com>
2017-07-20 15:57:23 +0200

    initial commit

--- app/src/main/java/com/example/android/psgameinventory/GameCursorAdapter.java
+++ app/src/main/java/com/example/android/psgameinventory/GameCursorAdapter.java
@@ -27,21 +27,28 @@ public class GameCursorAdapter extends CursorAdapter {
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+        TextView genreTextView = (TextView) view.findViewById(R.id.genre);
+        TextView consoleTextView= (TextView)view.findViewById(R.id.console);
+
 
         // Find the columns of pet attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(GameEntry.COLUMN_GAME_NAME);
         int gameColumnIndex = cursor.getColumnIndex(GameEntry.COLUMN_GAME_STOCK);
+        int genreColumnIndex=cursor.getColumnIndex(GameEntry.COLUMN_GAME_GENRE);
+        int consoleColumnIndex=cursor.getColumnIndex(GameEntry.COLUMN_GAME_CONSOLE);
 
         // Read the pet attributes from the Cursor for the current pet
         String gameName = cursor.getString(nameColumnIndex);
         String gameStock = cursor.getString(gameColumnIndex);
+        String gameGenre = cursor.getString(genreColumnIndex);
+        String gameConsole = cursor.getString(consoleColumnIndex);
 
-        // If the pet breed is empty string or null, then use some default text
-        // that says "Unknown breed", so the TextView isn't blank.
         if (TextUtils.isEmpty(gameStock)) {
-            gameStock = context.getString(R.string.unknown_stock);
+            gameStock = context.getString(R.string.unknown_Price);
         }
         // Update the TextViews with the attributes for the current pet
         nameTextView.setText(gameName);
         summaryTextView.setText(gameStock);
+        genreTextView.setText(gameGenre);
+        consoleTextView.setText(gameConsole);
     }}
